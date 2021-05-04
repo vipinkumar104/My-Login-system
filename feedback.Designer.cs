5292ffa86748dacabb42ddd5d0f1215c5ea525c7
vipinkumar104 <83219088+vipinkumar104@users.noreply.github.com>
2021-05-04 05:13:51 +0100

    Add files via upload

---
+++ feedback.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace Loginsystem
+{
+    partial class feedback
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button27 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(435, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 58);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Jobs Search";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Creative";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 28);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Leadership";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 28);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Hardworking";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(328, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 28);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Coding Skills";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(328, 294);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 28);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Work Expreance";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(328, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(157, 28);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Puntual";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(680, 415);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(92, 23);
+            this.button7.TabIndex = 31;
+            this.button7.Text = "Save";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(849, 415);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(92, 23);
+            this.button8.TabIndex = 32;
+            this.button8.Text = "Back";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(479, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 22);
+            this.textBox1.TabIndex = 58;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(331, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "Applicant Name";
+            // 
+            // button27
+            // 
+            this.button27.Location = new System.Drawing.Point(651, 70);
+            this.button27.Name = "button27";
+            this.button27.Size = new System.Drawing.Size(75, 23);
+            this.button27.TabIndex = 72;
+            this.button27.Text = "Select";
+            this.button27.UseVisualStyleBackColor = true;
+            this.button27.Click += new System.EventHandler(this.button27_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(531, 415);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(92, 23);
+            this.button16.TabIndex = 73;
+            this.button16.Text = "Clear all";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(39, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(251, 311);
+            this.richTextBox1.TabIndex = 74;
+            this.richTextBox1.Text = "";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(729, 135);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 94;
+            this.button10.Text = "Select";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(729, 176);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 95;
+            this.button11.Text = "Select";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(729, 220);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 96;
+            this.button12.Text = "Select";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(729, 258);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 97;
+            this.button13.Text = "Select";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(729, 299);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 98;
+            this.button14.Text = "Select";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(729, 335);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 99;
+            this.button15.Text = "Select";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Good",
+            "Bad"});
+            this.comboBox1.Location = new System.Drawing.Point(564, 134);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 100;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Good",
+            "Bad"});
+            this.comboBox2.Location = new System.Drawing.Point(564, 176);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 101;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Good",
+            "Bad"});
+            this.comboBox3.Location = new System.Drawing.Point(564, 217);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 102;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Good",
+            "Bad"});
+            this.comboBox4.Location = new System.Drawing.Point(564, 259);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 103;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Good",
+            "Bad"});
+            this.comboBox5.Location = new System.Drawing.Point(564, 299);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 24);
+            this.comboBox5.TabIndex = 104;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "Good",
+            "Bad"});
+            this.comboBox6.Location = new System.Drawing.Point(564, 339);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 24);
+            this.comboBox6.TabIndex = 105;
+            // 
+            // feedback
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1060, 450);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button27);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Name = "feedback";
+            this.Load += new System.EventHandler(this.feedback_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button27;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+    }
+}
---
+++ feedback.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Loginsystem
+{
+    public partial class feedback : Form
+    {
+        public feedback()
+        {
+            InitializeComponent();
+        }
+
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Rohit\source\repos\DB\LoginInfo2.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd1;
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+           
+        public static string creative=string.Empty;
+        public static string leadership= string.Empty;
+        public static string hardwork= string.Empty;
+        public static string punctual= string.Empty;
+        public static string workexpreance= string.Empty;
+        public static string coading=string.Empty;
+       
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter the applicant name");
+            }
+            else
+            {
+                richTextBox1.AppendText(textBox1.Text + "\n");
+            }
+        }
+
+       
+
+       
+       
+
+      
+
+      
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void feedback_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+       
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text=="Good")
+            {
+                richTextBox1.AppendText("Yoy are good creative thinker");
+            }
+            else
+            {
+                richTextBox1.AppendText("You are bad creative thinker");
+            }
+            }
+
+          
+       
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if(comboBox2.Text=="Good")
+            {
+                richTextBox1.AppendText("Your leadership skills are good");
+            }
+            else
+            {
+                richTextBox1.AppendText("Yoy are bad at leadership");
+            }
+           
+            
+
+            //////////////////////////////////////////////////
+
+            //////////////////////////////////////////
+            ///
+           
+        }
+      
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if(comboBox3.Text=="Good")
+            {
+                richTextBox1.AppendText("You are Hard Working");
+            }
+            else
+            {
+                richTextBox1.AppendText("Yoyare not a hard worker");
+            }
+            ////////////////////////
+            ///
+         
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if(comboBox5.Text=="Good")
+            {
+                richTextBox1.AppendText("You have good working expirience");
+            }
+            else
+            {
+                richTextBox1.AppendText("You dont have working expirence");
+            }
+        }
+
+      
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            ////////////////////////////////////////////////////////
+
+            con.Open();
+            cmd1 = new SqlCommand("INSERT INTO feedbk(nameuser,creative,leadership,hardwork,punctual, work,coding) VALUES (@nameuser,@creative,@leadership,@hardwork,@punctual,@work,@coding)", con);
+            cmd1.Parameters.AddWithValue("@nameuser", textBox1.Text.ToString());
+            cmd1.Parameters.AddWithValue("@creative", comboBox1.Text.ToString());
+            cmd1.Parameters.AddWithValue("@Leadership", comboBox2.Text.ToString());
+            cmd1.Parameters.AddWithValue("@hardwork", comboBox3.Text.ToString());
+            cmd1.Parameters.AddWithValue("@punctual", comboBox4.Text.ToString());
+            cmd1.Parameters.AddWithValue("@work", comboBox5.Text.ToString());
+            cmd1.Parameters.AddWithValue("@coding", comboBox6.Text.ToString());
+            cmd1.ExecuteNonQuery();
+            con.Close();
+
+
+            MessageBox.Show($"Applicant {textBox1.Text} Feedback saved");
+
+
+            this.Hide();
+            Show_feedback shfeed = new Show_feedback();
+            shfeed.ShowDialog();
+            this.Close();
+
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if(comboBox4.Text=="Good")
+            {
+                richTextBox1.AppendText("You are not punctual");
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if(comboBox6.Text=="Good")
+            {
+                richTextBox1.AppendText("You have good coading skills");
+            }
+            else
+            {
+                richTextBox1.AppendText("You have bad coading skills");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
